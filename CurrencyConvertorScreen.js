e9cb8bfe244c137bf517cdcf3f98711c53c58da5
Ali Mohammadi <alimohammadi@Alis-MacBook-Pro.local>
2025-04-30 11:57:19 +1000

    API works and the received data appear in the console

--- CurrencyConvertorScreen.js
+++ CurrencyConvertorScreen.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { use, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
+
 const CurrencyConverterScreen = () => {
+  useEffect(() => {
+    const fetchExchangeRates = async () => {
+      const rates = await fetchLatestExchangeRates();
+      console.log(rates);
+    };
+  
+    fetchExchangeRates();
+  }
+  , []);
+  const fetchLatestExchangeRates = async () => {
+    try {
+      const response = await fetch(
+        'https://openexchangerates.org/api/latest.json?app_id=e954581353f94a19b67f3f2acaf6f6ed');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching exchange rates:', error);
+    }
+  };
   return (
     <View>
       <Text>Currency Converter</Text>
