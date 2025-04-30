68425629c52c085540b9b8e29e08112282217e43
Ali Mohammadi <alimohammadi@Alis-MacBook-Pro.local>
2025-04-30 11:52:48 +1000

    first screen created and path modified

---
+++ CurrencyConvertorScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CurrencyConverterScreen = () => {
+  return (
+    <View>
+      <Text>Currency Converter</Text>
+    </View>
+  );
+};
+
+
+
+export default CurrencyConverterScreen;
